75e225531fd3b1a2bac29cf11eb72b7f3378dfb4
Aahlad <aahladkethineeedi@gmail.com>
2023-12-03 20:36:08 +0530

    Added Config for Redis Client Connection

--- config/app.config.js
+++ config/app.config.js
@@ -1,20 +1,25 @@
-const redis = require("redis");
-const axios = require("axios");
-const express = require("express");
-// Create a client
+import { createClient } from 'redis';
 
-let client = redis.createClient();
+
+const client = createClient({
+    password: 'root12345',
+    socket: {
+        host: 'redis-19245.c267.us-east-1-4.ec2.cloud.redislabs.com',
+        port: 19245
+    }
+})
 
 client.on('connect', () => {
-    console.log("Redis connection established...")
+    console.log('Redis Client connected...');
 })
 
-client.on('error', () => {
-    console.log("An error occured connecting to Redis!")
+client.on('error', err => console.log('Error Connecting to Redis Client :', err));
+
+console.log('connecting...')
+await client.connect();
+
+process.on('SIGINT', async () => {
+    console.log("  Graceful shutdown started...")
+    await client.quit();
 })
 
-
-// Set Port number
-const portNumber = 8080
-
-const app = express();
